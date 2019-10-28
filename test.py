5bff365efebe1d25d59a8ffa0333d88cef400043
dravin_nagalingam <dravin.nagalingam@gmail.com>
2019-10-28 14:46:10 -0400

    another face

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ import numpy
 import cv2
 result = cv2.matchTemplate(cv2.imread("./whereIs/where.jpg"), cv2.imread("./Faces/face.png"), cv2.TM_CCOEFF_NORMED)
 print(max(map(max, result)))
+
+box = cv2.rectangle('where', )
