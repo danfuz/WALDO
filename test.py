e1df1f53e5a64ec07271aa994a0a33b1b9651981
danfuz <danryafuz@gmail.com>
2019-10-28 14:45:13 -0400

    k

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 import numpy
 import cv2
 result = cv2.matchTemplate(cv2.imread("./whereIs/where.jpg"), cv2.imread("./Faces/face.png"), cv2.TM_CCOEFF_NORMED)
-print(max(map(max, result)))
+maxx = max(map(max, result))
+where = numpy.where(result == maxx)
+y = int(where[0])
+x = int(where[1])
+
+
+img = cv2.imread("whereIs/where.jpg")
+cv2.rectangle(img, (x-2,y-2),(x+62, y+62),(0,0,0), 3)
+cv2.imshow("s",img)
+cv2.waitKey(0)
+
