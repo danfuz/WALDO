79031d010ed8c3b378c46cac23e4a8e81c75e10e
danfuz <danryafuz@gmail.com>
2019-10-28 14:19:00 -0400

    k

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-#ffffffffffg
-
+import numpy
+import cv2
+result = cv2.matchTemplate(cv2.imread("./whereIs/where.jpg"), cv2.imread("./Faces/face.png"), cv2.TM_CCOEFF_NORMED)
+print(max(map(max, result)))
